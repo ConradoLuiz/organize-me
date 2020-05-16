46d699d3af2968cf2b427b9743ddaacf881dd9a3
ConradoLuiz <conradol.vargas@gmail.com>
2020-05-16 20:12:04 -0300

    test frontend

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
 let serviceAccount = require('../todo-mvc-c5999-42cd687704ca.json');
 
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-let db = admin.firestore();
+const db = admin.firestore();
 
 module.exports = db;
