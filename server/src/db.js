11fcc5e463517f1d9bda707067d5e4cf675e02cb
ConradoLuiz <conradol.vargas@gmail.com>
2020-05-18 13:40:54 -0300

    hide private key

--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
 let serviceAccount = require('../todo-mvc-c5999-42cd687704ca.json');
 
+serviceAccount.private_key = process.env.PRIVATE_KEY
+serviceAccount.private_key_id = process.env.PRIVATE_KEY_ID
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
