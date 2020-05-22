2df0e029ac8bc3bb15322c41ef2c5a13c8f41905
ConradoLuiz <conradol.vargas@gmail.com>
2020-05-22 01:58:16 -0300

    fixed notes list

--- web/src/components/Note/index.js
+++ web/src/components/Note/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
-
+import { FiTrash, FiCheck } from 'react-icons/fi';
 import styles from './styles.css';
 
 export default function Note({ title, description, created_at}) {
     return (
         <div className='note'>
-            <h4>
+            <h4 className='note-title'>
                 <strong>
                     { title }
                 </strong>
             </h4>
 
-            <h5>{ description }</h5>
-            <h6>Criada em { created_at }</h6>
+            <h5 className='note-description'>{ description }</h5>
+            <h6 className='note-created'>Criada em { created_at }</h6>
 
+            <div className="buttons">
+                <button className='note-delete'>
+                    <FiTrash size={18} />
+                </button>
+                <button className='note-complete'>
+                    <FiCheck size={18} />
+                </button>
+            </div>
         </div>
     )
 }
--- web/src/components/NotesDisplay/index.js
+++ web/src/components/NotesDisplay/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import NotesList from '../NotesList';
+// import NotesList from '../NotesList';
 import logo from '../../assets/logo.svg';
 import styles from './styles.css';
+
+import { FiPlusCircle } from 'react-icons/fi';
+
+import Note from '../Note';
 
 export default function NotesDisplay() {
     return (
@@ -15,7 +19,28 @@ export default function NotesDisplay() {
                 <h2>Olá, Conrado</h2>
             </header>
 
-            <NotesList/>
+            
+            <div className="add-note">
+                <p>Selecione uma tarefa ou crie uma nova</p>
+
+                <FiPlusCircle size={24} color='#370588' />
+            </div>
+
+            <div className="notes-container">
+                <Note title={'Im a note'} description={'Simple description of a note'} created_at={'21/05/2020 às 19:43h'}/>
+
+                <Note 
+                    title={'Im a note'} 
+                    description={'Simple description of a note, but these can get very complex, very quickly and they grow'} 
+                    created_at={'21/05/2020 às 19:43h'}
+                />
+                <Note title={'Im a note'} description={''} created_at={'21/05/2020 às 19:43h'}/>
+                <Note title={'Im a note'} description={'Simple description of a note'} created_at={'21/05/2020 às 19:43h'}/>
+                <Note title={'Im a note'} description={'Simple description of a note'} created_at={'21/05/2020 às 19:43h'}/>
+                <Note title={'Im a note'} description={'Simple description of a note'} created_at={'21/05/2020 às 19:43h'}/>
+                
+            </div>
+        
         </div>
     )
 }
