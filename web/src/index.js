88d74cd95f5ce656ab884abc869e3d56bfb24ec0
ConradoLuiz <conradol.vargas@gmail.com>
2020-05-27 19:14:07 -0300

    index fix

--- web/src/index.js
+++ web/src/index.js
@@ -5,9 +5,11 @@ import { hydrate, render } from "react-dom";
 import App from './App';
 
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
+
+ReactDOM.render( <App />, document.getElementById("root"));
